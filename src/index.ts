4c6ea350a805a9d130808cc280be3487504cf18c
7oxP <92915640+7oxP@users.noreply.github.com>
2023-09-17 19:08:50 +0700

    adding upload and delete feature

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-import dotenv  from 'dotenv'
+// import dotenv  from 'dotenv'
+import { IUploadedFile } from './contracts/IFile';
+import { StorageService } from './usecases/storage/StorageService'
+// dotenv.config()
 
-dotenv.config()
+const storage = new StorageService()
+storage.deleteFile('gs://smartcube-0101/testFiles.txt')
+
+const fileData = 'Ini adalah data file yang ingin Anda simpan dalam buffer.';
+const fileName = 'nama-file.txt';
+const fileType = 'text/plain';
+
+
+const file: IUploadedFile = {
+  buffer: Buffer.from(fileData),
+  originalname: fileName,
+  mimetype: fileType, 
+};
+
+storage.uploadFile(file)
+
+console.log(file)
--- src/usecases/storage/StorageService.ts
+++ src/usecases/storage/StorageService.ts
@@ -2,18 +2,61 @@ import { IResponse } from "@/contracts/usecases/IResponse";
 import { IStorageService } from "@/contracts/usecases/IStorageServices";
 import { Response } from "../../utils/Response";
 import { IUploadedFile } from "@/contracts/IFile";
+import { Storage } from "@google-cloud/storage";
 
 export class StorageService implements IStorageService {
-    
-    uploadFile(file: IUploadedFile): IResponse {
-        return new Response()
-        .setStatus(true)
-        .setStatusCode(1)
-        .setMessage("ok")
-        .setData({fileUrl: "https://google.apis/image1.jpg"}) //dummy url
+  uploadFile(file: IUploadedFile): IResponse {
+
+    const path = require("path");
+    const keyFilename = "src/studied-union-399214-f9386348b285.json";
+    const bucketName = "smartcube-0101";
+    const filePath = "src/testFiles.txt";
+    const destFileName = path.basename(filePath);
+
+    const storage = new Storage({ keyFilename });
+
+    async function uploadingFile() {
+      const options = {
+        destination: destFileName,
+      };
+
+      await storage.bucket(bucketName).upload(filePath, options);
+
+      console.log(`${destFileName} uploaded to ${bucketName}`);
     }
 
-    deleteFile(fileUrl: string): IResponse {
-        throw new Error("Method not implemented.");
+    uploadingFile().catch(console.error);
+
+    return new Response()
+      .setStatus(true)
+      .setStatusCode(1)
+      .setMessage("ok")
+      .setData({ fileName: destFileName, bucketName: bucketName });
+  }
+
+  deleteFile(fileUrl: string): IResponse {
+    const { Storage } = require("@google-cloud/storage");
+
+    const bucketData = fileUrl.split("/");
+
+    const bucketName = bucketData[2];
+    const fileName = bucketData[3];
+    const keyFilename = "src/studied-union-399214-f9386348b285.json";
+
+    const storage = new Storage({ keyFilename });
+
+    async function deleteFile() {
+      await storage.bucket(bucketName).file(fileName).delete();
+
+      console.log(`${fileUrl} deleted`);
     }
+
+    deleteFile().catch(console.error);
+
+    return new Response()
+      .setStatus(true)
+      .setStatusCode(1)
+      .setMessage("ok")
+      .setData({ fileName: fileName, bucketName: bucketName });
+  }
 }
