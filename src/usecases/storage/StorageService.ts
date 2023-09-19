7ffa4135579e51f7ce83e20a00b91743e2ff448d
7oxP <92915640+7oxP@users.noreply.github.com>
2023-09-19 13:19:54 +0700

    load credential and bucket name from environment variable

--- src/usecases/storage/StorageService.ts
+++ src/usecases/storage/StorageService.ts
@@ -1,19 +1,19 @@
-import { IResponse } from "@/contracts/usecases/IResponse";
-import { IStorageService } from "@/contracts/usecases/IStorageServices";
-import { Response } from "../../utils/Response";
-import { IUploadedFile } from "@/contracts/IFile";
-import { Storage } from "@google-cloud/storage";
-import path from "path";
-import { OperationStatus } from "../../constants/operations";
+import { IResponse } from "@/contracts/usecases/IResponse"
+import { IStorageService } from "@/contracts/usecases/IStorageServices"
+import { Response } from "../../utils/Response"
+import { IUploadedFile } from "@/contracts/IFile"
+import { Storage } from "@google-cloud/storage"
+import { OperationStatus } from "../../constants/operations"
+import "dotenv/config"
 
 export class StorageService implements IStorageService {
-  private storage: Storage;
-  private bucketName: string;
+  private storage: Storage
+  private bucketName: string
 
   constructor() {
-    const keyFilename = "service_accounts/google_service_account.json";
-    this.storage = new Storage({ keyFilename });
-    this.bucketName = "smartcube-0101";
+    const keyFilename = `${process.env.GOOGLE_APPLICATION_CREDENTIALS}`
+    this.storage = new Storage({ keyFilename })
+    this.bucketName = `${process.env.BUCKET_NAME}`
   }
 
   async uploadFile(file: IUploadedFile): Promise<IResponse> {
@@ -21,9 +21,9 @@ export class StorageService implements IStorageService {
       await this.storage
         .bucket(this.bucketName)
         .file(file.originalname)
-        .save(file.buffer);
+        .save(file.buffer)
 
-      console.log(`${file.originalname} uploaded to ${this.bucketName}`);
+      console.log(`${file.originalname} uploaded to ${this.bucketName}`)
 
       return new Response()
         .setStatus(true)
@@ -33,38 +33,36 @@ export class StorageService implements IStorageService {
           fileUrl: `https://storage.cloud.google.com/${this.bucketName}/${file.originalname}`,
           fileName: file.originalname,
           bucketName: this.bucketName,
-        });
+        })
     } catch (error: any) {
       return new Response()
         .setStatus(false)
         .setStatusCode(OperationStatus.cloudStorageError)
         .setMessage(error)
-        .setData({});
+        .setData({})
     }
   }
 
   async deleteFile(fileUrl: string): Promise<IResponse> {
-    const bucketData = fileUrl.split("/");
+    const bucketData = fileUrl.split("/")
 
-    const fileName = bucketData[4];
+    const fileName = bucketData[4]
 
     try {
-      await this.storage.bucket(this.bucketName).file(fileName).delete();
-      console.log(`${fileUrl} deleted`);
+      await this.storage.bucket(this.bucketName).file(fileName).delete()
+      console.log(`${fileUrl} deleted`)
 
       return new Response()
         .setStatus(true)
         .setStatusCode(OperationStatus.success)
         .setMessage("ok")
-        .setData({ fileName: fileName, bucketName: this.bucketName });
-
+        .setData({ fileName: fileName, bucketName: this.bucketName })
     } catch (error: any) {
-
       return new Response()
         .setStatus(false)
         .setStatusCode(OperationStatus.cloudStorageError)
         .setMessage(error)
-        .setData({});
+        .setData({})
     }
   }
 }
