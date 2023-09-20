398553d13ac242db4e0a9fb0a09ba66ee8608bed
IyanTRICKSTER <akiyan2002@gmail.com>
2023-09-20 15:55:26 +0700

    fix dummy data for integration test in NotificationService.test

--- tests/integrations/NotificationService.test.ts
+++ tests/integrations/NotificationService.test.ts
@@ -14,15 +14,40 @@ import { IUploadedFile } from "../../src/contracts/IFile";
 import { OperationStatus } from "../../src/constants/operations";
 import { AuthGuard } from "../../src/middleware/AuthGuard";
 import { UserRoles } from "../../src/contracts/middleware/AuthGuard";
+import { Database } from "../../src/database/db";
 
 dotenv.config();
+
+let db: Database = new Database(
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  process.env.DB_HOST!,
+  process.env.DB_PORT!,
+  process.env.DB_NAME!,
+)
 
 let notificationRepository: INotificationRepositories;
 let cloudStorageService: IStorageService;
 let cloudMessageService: ICloudMessagingService;
 let notificationService: INotificationService;
 
+
+
+
 beforeAll(async () => {
+
+  //Connect db
+  db.connect()
+
+  //create dummy user with id 1
+  db.getConnection().query('DELETE FROM notifications')
+  db.getConnection().query('DELETE FROM users WHERE id = 1000')
+
+  db.getConnection().query("INSERT INTO users (id, username, email, password) VALUES (1000, 'iyan', 'iyan@mail.com', 'pass123')")
+  db.getConnection().query("INSERT INTO notifications (id, user_id, title, image, description, is_viewed, created_at, updated_at) VALUES " +
+    `(1000,  1000, 'title 1', 'image 1', 'description 1', 0, '2023-09-09', '2023-09-09')`)
+
+  //Instantiate services
   notificationRepository = new NotificationRepository();
   cloudStorageService = new StorageService();
   cloudMessageService = new CloudMessagingService();
@@ -33,6 +58,12 @@ beforeAll(async () => {
   );
 });
 
+afterAll(() => {
+  db.getConnection().query('DELETE FROM notifications')
+  db.getConnection().query('DELETE FROM users WHERE id = 1000')
+})
+
+
 const filePath = process.cwd() + "/tests/images/img1.png";
 const buffer = fs.readFileSync(filePath);
 const file: IUploadedFile = {
@@ -41,40 +72,59 @@ const file: IUploadedFile = {
   mimetype: "image/png",
 };
 
-describe("store notification", () => {
-  it("Store success", async () => {
-    const authGuard = new AuthGuard(1, "", "", UserRoles.Admin);
+let storedNotificationID = 0
 
-    const res = await notificationService.storeNotification(
-      authGuard,
-      file,
-      "wew1",
-      "wew1 desc"
-    );
-    assert.ok(res.getStatus());
-    assert.equal(res.getStatusCode(), OperationStatus.success);
-  }, 10000);
+
+describe("store notifs", () => {
 
   it("Store failed with user id invalid", async () => {
+
+    //create auth guard
     const authGuard = new AuthGuard(0, "", "", UserRoles.Admin); //User with id 0 is invalid or not exists
 
+    //execute usecase
+    const resp = await notificationService.storeNotification(
+      authGuard,
+      file,
+      "wew1",
+      "wew1 desc"
+    );
+
+    //assert
+    assert.equal(resp.getStatus(), false);
+    assert.equal(resp.getStatusCode(), OperationStatus.repoError);
+
+  });
+
+  it("Store success", async () => {
+
+    //create auth guard
+    const authGuard = new AuthGuard(1000, "iyan@mail.com", "iyan", UserRoles.Admin);
+
+    //execute usecase
     const res = await notificationService.storeNotification(
       authGuard,
       file,
       "wew1",
       "wew1 desc"
     );
-    assert.equal(res.getStatus(), false);
-    assert.equal(res.getStatusCode(), OperationStatus.repoError);
+
+    //assert
+    assert.ok(res.getStatus());
+    assert.equal(res.getStatusCode(), OperationStatus.success);
+
+    storedNotificationID = res.getData().id
   });
 });
 
 describe("view notification", () => {
-  it("success", async () => {
-    const authGuard = new AuthGuard(1, "", "", UserRoles.Admin);
 
-    const res = await notificationService.viewNotification(authGuard, 2);
-    console.log(res.getMessage());
+  it("success", async () => {
+    const authGuard = new AuthGuard(1000, "", "", UserRoles.Admin);
+
+    const res = await notificationService.viewNotification(authGuard, storedNotificationID);
+    // console.log(res.getMessage());
+
     assert.ok(res.getStatus());
     assert.equal(res.getStatusCode(), OperationStatus.success);
   });
@@ -82,57 +132,58 @@ describe("view notification", () => {
   it("failed unauthorized", async () => {
     const authGuard = new AuthGuard(0, "", "", UserRoles.Admin);
 
-    const res = await notificationService.viewNotification(authGuard, 2);
-    console.log(res.getMessage());
+    const res = await notificationService.viewNotification(authGuard, storedNotificationID);
+    // console.log(res.getMessage());
+
     assert.equal(res.getStatus(), false);
     assert.equal(res.getStatusCode(), OperationStatus.unauthorizedAccess);
   });
 });
 
 describe("delete notification", () => {
-  const authGuard = new AuthGuard(1, "", "", UserRoles.Admin);
-
-  it("success", async () => {
-    //create dummy data
-    const storeRes = await notificationService.storeNotification(
-      authGuard,
-      file,
-      "wew1",
-      "wew1 desc"
-    );
-
-    //delete notif
-    const res = await notificationService.deleteNotification(
-      authGuard,
-      storeRes.getData().id
-    );
-    console.log(res.getMessage());
-
-    //assert
-    assert.ok(res.getStatus());
-    assert.equal(res.getStatusCode(), OperationStatus.success);
-  });
+  const authGuard = new AuthGuard(1000, "", "", UserRoles.Admin);
 
   it("failed unauthorized", async () => {
     //create dummy data
-    const storeRes = await notificationService.storeNotification(
-      authGuard,
-      file,
-      "wew1",
-      "wew1 desc"
-    );
+    // const storeRes = await notificationService.storeNotification(
+    //   authGuard,
+    //   file,
+    //   "wew1",
+    //   "wew1 desc"
+    // );
 
     //delete notif
     const authGuard2 = new AuthGuard(999999, "", "", UserRoles.Admin);
     const res = await notificationService.deleteNotification(
       authGuard2,
-      storeRes.getData().id
+      storedNotificationID
     );
     // console.log(res.getMessage())
 
     //assert
     assert.equal(res.getStatus(), false);
     assert.equal(res.getStatusCode(), OperationStatus.unauthorizedAccess);
+  });
+
+  it("success", async () => {
+    //create dummy data
+    // const storeRes = await notificationService.storeNotification(
+    //   authGuard,
+    //   file,
+    //   "wew1",
+    //   "wew1 desc"
+    // );
+
+    //delete notif
+    const res = await notificationService.deleteNotification(
+      authGuard,
+      storedNotificationID
+    );
+    console.log(res.getMessage());
+
+    //assert
+    assert.ok(res.getStatus());
+    assert.equal(res.getStatusCode(), OperationStatus.success);
   });
 
   it("failed model not found", async () => {
