55e921d67f96cf499457684626d842736acc6e21
IyanTRICKSTER <akiyan2002@gmail.com>
2023-09-22 23:49:19 +0700

    add unix socket db connection on server

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize"
 
 export class Database {
-    
+
     private user: string = ''
     private password: string = ''
     private host: string = ''
@@ -21,13 +21,19 @@ export class Database {
 
     connect(): void {
         try {
-            if (this.conn == null) {
+            if (process.env.APP_ENV == 'local') {
                 this.conn = new Sequelize(`${this.dialect}://${this.user}:${this.password}@${this.host}:${this.port}/${this.dbName}`)
-                this.conn.authenticate().then();
+                this.conn.authenticate()
+            } else {
+                this.conn = new Sequelize(this.dbName, this.user, this.password, {
+                    dialect: 'mysql',
+                    host: this.host
+                });
+                this.conn.authenticate()
             }
         } catch (error) {
             throw new Error('Unable to connect to the database: ' + error)
-        } 
+        }
     }
 
     getConnection(): Sequelize {
